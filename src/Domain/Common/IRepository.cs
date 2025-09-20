b71e972b1e5ee8e27655552e437af348b350973b
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-09-20 15:25:16 +0200

    Add base IRepository

---
+++ src/Domain/Common/IRepository.cs
@@ -0,0 +1,20 @@
+using System.Linq.Expressions;
+
+namespace Domain.Common;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    
+    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default);
+    
+    Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    
+    void Update(TEntity entity);
+    
+    void Remove(TEntity entity);
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
